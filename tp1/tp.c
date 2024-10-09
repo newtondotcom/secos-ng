c3e7fd8b181343309eed7fecc3d7814027f5daed
newtondotcom <asphalt9fr@gmail.com>
2024-10-09 17:24:35 +0200

    feat : tp1 q5-6-7

--- tp1/tp.c
+++ tp1/tp.c
@@ -61,4 +61,50 @@ void tp() {
     print_gdt_content(gdtr_ptr);
 
     print_segment_selector();
+
+    // L'adresse de base de la GDT doit être alignée sur 8 octets
+    seg_desc_t new_gdt[7];
+
+    // Le premier descripteur (indice 0) doit être NULL.
+    new_gdt[0].raw = 0ULL;
+
+    // Code, 32 bits RX, flat, indice 1
+    new_gdt[1].limit_1 = 0xffff;   //:16;     /* bits 00-15 of the segment limit */
+    new_gdt[1].base_1 = 0x0000;    //:16;     /* bits 00-15 of the base address */
+    new_gdt[1].base_2 = 0x00;      //:8;      /* bits 16-23 of the base address */
+    new_gdt[1].type = 11;//Code,RX //:4;      /* segment type */
+    new_gdt[1].s = 1;              //:1;      /* descriptor type */
+    new_gdt[1].dpl = 0; //ring0    //:2;      /* descriptor privilege level */
+    new_gdt[1].p = 1;              //:1;      /* segment present flag */
+    new_gdt[1].limit_2 = 0xf;      //:4;      /* bits 16-19 of the segment limit */
+    new_gdt[1].avl = 1;            //:1;      /* available for fun and profit */
+    new_gdt[1].l = 0; //32bits     //:1;      /* longmode */
+    new_gdt[1].d = 1;              //:1;      /* default length, depend on seg type */
+    new_gdt[1].g = 1;              //:1;      /* granularity */
+    new_gdt[1].base_3 = 0x00;      //:8;      /* bits 24-31 of the base address */
+
+    // Données, 32 bits RW, flat, indice 2.
+    new_gdt[2].limit_1 = 0xffff;   //:16;     /* bits 00-15 of the segment limit */
+    new_gdt[2].base_1 = 0x0000;    //:16;     /* bits 00-15 of the base address */
+    new_gdt[2].base_2 = 0x00;      //:8;      /* bits 16-23 of the base address */
+    new_gdt[2].type = 3; //data,RW //:4;      /* segment type */
+    new_gdt[2].s = 1;              //:1;      /* descriptor type */
+    new_gdt[2].dpl = 0; //ring0    //:2;      /* descriptor privilege level */
+    new_gdt[2].p = 1;              //:1;      /* segment present flag */
+    new_gdt[2].limit_2 = 0xf;      //:4;      /* bits 16-19 of the segment limit */
+    new_gdt[2].avl = 1;            //:1;      /* available for fun and profit */
+    new_gdt[2].l = 0; // 32 bits   //:1;      /* longmode */
+    new_gdt[2].d = 1;              //:1;      /* default length, depend on seg type */
+    new_gdt[2].g = 1;              //:1;      /* granularity */
+    new_gdt[2].base_3 = 0x00;      //:8;      /* bits 24-31 of the base address */
+
+    printf("Nouvelle GDT : %lu\n", (long unsigned int)new_gdt);
+
+
+    set_gdtr(new_gdt);
+    gdt_reg_t new_gdtr_ptr;
+    // get_gdtr(new_gdtr_ptr); -- MARCHE PAS
+    new_gdtr_ptr.addr = (long unsigned int)new_gdt;
+    new_gdtr_ptr.limit = sizeof(new_gdt) -1;
+    print_gdt_content(new_gdtr_ptr);
 }
