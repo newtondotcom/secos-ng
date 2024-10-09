99f71b3362aa86df346956dd5d28977fd0e2e8eb
newtondotcom <asphalt9fr@gmail.com>
2024-10-09 11:20:23 +0200

    feat : rep q2

--- tp0/tp.c
+++ tp0/tp.c
@@ -13,7 +13,23 @@ void tp() {
    multiboot_memory_map_t* entry = (multiboot_memory_map_t*)info->mbi->mmap_addr;
    while((uint32_t)entry < (info->mbi->mmap_addr + info->mbi->mmap_length)) {
       // TODO print "[start - end] type" for each entry
-
+      string type;
+      switch ( entry->type ) {
+         case 1:
+         type = "MULTIBOOT_MEMORY_AVAILABLE";
+            break;
+         case 2:
+         type = "MULTIBOOT_MEMORY_RESERVED";
+            break;
+         case 3:
+         type = "MULTIBOOT_MEMORY_ACPI_RECLAIMABLE";
+            break;
+         case 4:
+         type = "MULTIBOOT_MEMORY_NVS";
+         default:
+            printf("MEMORY CARTOGRAPHY FAILED");
+      }
+    printf(" [0x%llx - 0x%llx] %s\n", entry->addr, entry->addr + entry->len, type);
       entry++;
    }
 
