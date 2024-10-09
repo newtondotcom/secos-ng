31b0a633dad19a24105e5aedbe71a5bfefdcf930
newtondotcom <asphalt9fr@gmail.com>
2024-10-09 10:57:38 +0200

    build : setup script to run each time on school computer

---
+++ setup.sh
@@ -0,0 +1,5 @@
+git config --global user.name "newtondotcom"
+git config --global user.email "asphalt9fr@gmail.com"
+sudo snap install code --classic
+sudo apt-get install qemu-system-x86 qemu-kvm gcc-multilib make git
+code .
