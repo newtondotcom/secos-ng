9f5344640f9b6654385b55f74d18f4f552c29443
newtondotcom <asphalt9fr@gmail.com>
2024-10-09 11:30:53 +0200

    fix : req q2 with -1 offset

--- tp0/tp.c
+++ tp0/tp.c
@@ -2,35 +2,37 @@
 #include <debug.h>
 #include <info.h>
 
-extern info_t   *info;
+extern info_t *info;
 extern uint32_t __kernel_start__;
 extern uint32_t __kernel_end__;
 
-void tp() {
+void tp()
+{
    debug("kernel mem [0x%p - 0x%p]\n", &__kernel_start__, &__kernel_end__);
    debug("MBI flags 0x%x\n", info->mbi->flags);
 
-   multiboot_memory_map_t* entry = (multiboot_memory_map_t*)info->mbi->mmap_addr;
-   while((uint32_t)entry < (info->mbi->mmap_addr + info->mbi->mmap_length)) {
-      // TODO print "[start - end] type" for each entry
+   multiboot_memory_map_t *entry = (multiboot_memory_map_t *)info->mbi->mmap_addr;
+   while ((uint32_t)entry < (info->mbi->mmap_addr + info->mbi->mmap_length))
+   {
       string type;
-      switch ( entry->type ) {
-         case 1:
+      switch (entry->type)
+      {
+      case 1:
          type = "MULTIBOOT_MEMORY_AVAILABLE";
-            break;
-         case 2:
+         break;
+      case 2:
          type = "MULTIBOOT_MEMORY_RESERVED";
-            break;
-         case 3:
+         break;
+      case 3:
          type = "MULTIBOOT_MEMORY_ACPI_RECLAIMABLE";
-            break;
-         case 4:
+         break;
+      case 4:
          type = "MULTIBOOT_MEMORY_NVS";
-         default:
-            printf("MEMORY CARTOGRAPHY FAILED");
+         break;
+      default:
+         printf("MEMORY CARTOGRAPHY FAILED");
       }
-    printf(" [0x%llx - 0x%llx] %s\n", entry->addr, entry->addr + entry->len, type);
+      printf(" [0x%llx - 0x%llx] %s\n", entry->addr, entry->addr + entry->len - 1, type);
       entry++;
    }
-
 }
