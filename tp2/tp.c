a071dc10e6c3887ee7ee258ea448e18cd76a04a4
newtondotcom <asphalt9fr@gmail.com>
2024-10-23 06:30:39 +0000

    feat : tp2 q1

--- tp2/tp.c
+++ tp2/tp.c
@@ -1,5 +1,6 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
+#include <intr.h>
 
 void bp_handler() {
    // TODO
@@ -11,6 +12,9 @@ void bp_trigger() {
 
 void tp() {
 	// TODO print idtr
+	idt_reg_t idtr;
+	get_idtr(idtr);
+	debug("%d\n",(unsigned int)idtr.addr);
 
 	// TODO call bp_trigger
    //bp_trigger();
